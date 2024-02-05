2dce1408a749dae744b980f223975b9554365b4b
Shubham <shubgoyal1998@gmail.com>
2024-02-05 15:15:31 +0530

    form fix

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,14 +4,14 @@ function Contact() {
    return (
       <section className="bg-white dark:bg-gray-900">
          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-lime-500">
                Contact Us
             </h2>
             <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
                Got a technical issue? Want to send feedback about a beta
                feature? Need details about our Business plan? Let us know.
             </p>
-            <form action="#" className="space-y-8" name="contact" method="POST" data-netlify="true">
+            <form className="space-y-8" name="contact" method="POST" data-netlify="true">
                <div>
                   <label
                      htmlFor="email"
